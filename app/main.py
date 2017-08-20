3272cf80dc8b8bd1e7a1837850bd0fa34eb44420
Joel Cressy <joel@jtcressy.net>
2017-08-20 01:47:08 -0600

    env var error handling, can set openshift api server via env var

--- app/main.py
+++ app/main.py
@@ -7,6 +7,25 @@ import subprocess
 client = discord.Client()
 oc_config_folder = "/data/kubeconfigs/"
 oc_config_arg = "--config " + oc_config_folder
+try:
+    discord_api_token = os.environ['DISCORD_API_TOKEN']
+except KeyError as err:
+    print("ERROR: API token required for Discord API via env var: DISCORD_API_TOKEN \n Details: \n")
+    print(err)
+
+try:
+    oc_server_fqdn = os.environ['OPENSHIFT_API_FQDN']
+except KeyError as err:
+    print("ERROR: No OpenShift API server defined via environment variable: OPENSHIFT_API_FQDN \n Details: \n")
+    print(err)
+
+try: 
+    oc_server_port = os.environ['OPENSHIFT_API_PORT']
+except KeyError as err:
+    print("WARN: OPENSHIFT_API_PORT not defined. Details: \n")
+    print(err)
+    pass
+
 
 @client.event
 async def on_ready():
@@ -23,12 +42,16 @@ async def on_message(message):
             if isinstance(message.channel, discord.PrivateChannel):
                 if len(args) == 3:
                     octoken = args[2]
-                    await client.send_message(message.channel, "```" + execute("oc " + oc_config_arg + message.author.id + " login --token " + octoken + " cloud.jtcressy.net") + "```")
+                    await client.send_message(message.channel, "```" + execute("oc " + oc_config_arg + message.author.id + " login --token " + octoken + " " + oc_server_fqdn + ":" + oc_server_port) + "```")
+                elif len(args) == 4:
+                    octoken = args[2]
+                    ocurl = args[3]
+                    await client.send_message(message.channel, "```" + execute("oc " + oc_config_args + message.author.id + " login --token " + octoken + " " + ocurl) + "```")
                 else:
                     await client.send_message(message.channel, "You need to supply a token with ``!oc login <token>`` by requesting one from https://cloud.jtcressy.net/oauth/token/request")
             else:
                 await client.send_message(message.author, "You cannot login to OpenShift tools using a public channel. Send me a private message with ``!oc login <token>`` instead.")
-                await client.send_message(message.author, "You need to supply a token with ``!oc login <token>`` by requesting one from https://cloud.jtcressy.net/oauth/token/request")
+                await client.send_message(message.author, "You need to supply a token with ``!oc login <token>`` by requesting one from https://" + oc_server_fqdn + ":" + oc_server_port + "/oauth/token/request")
                 await client.delete_message(message)
         elif args[1] == "status":
             await client.send_message(message.channel, message.author.mention + "```" + execute("oc " + oc_config_arg + message.author.id + " status") + "```")
@@ -46,4 +69,4 @@ def execute(command):
     output, err = p.communicate()
     return output.decode('utf-8')
 
-client.run(os.environ['DISCORD_API_TOKEN'])
+client.run(discord_api_token)
